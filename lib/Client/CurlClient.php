a48a98bc903f2d1e109accff9ae2425e9022d1b8
Антон Н. Николаев <nikolaevanym@yamoney.ru>
2019-08-23 11:18:14 +0300

    add UserAgent

--- lib/Client/CurlClient.php
+++ lib/Client/CurlClient.php
@@ -31,7 +31,6 @@ use YandexCheckout\Common\Exceptions\ApiConnectionException;
 use YandexCheckout\Common\Exceptions\ApiException;
 use YandexCheckout\Common\Exceptions\AuthorizeException;
 use YandexCheckout\Common\Exceptions\ExtensionNotFoundException;
-use YandexCheckout\Common\HttpVerb;
 use YandexCheckout\Common\ResponseObject;
 use YandexCheckout\Helpers\RawHeadersParser;
 
@@ -76,6 +75,9 @@ class CurlClient implements ApiClientInterface
      */
     private $proxy;
 
+    /** @var UserAgent */
+    private $userAgent;
+
     /**
      * @var bool
      */
@@ -98,6 +100,14 @@ class CurlClient implements ApiClientInterface
      * @var LoggerInterface|null
      */
     private $logger;
+
+    /**
+     * CurlClient constructor.
+     */
+    public function __construct()
+    {
+        $this->userAgent = new UserAgent();
+    }
 
     /**
      * @param LoggerInterface|null $logger
@@ -310,6 +320,14 @@ class CurlClient implements ApiClientInterface
     }
 
     /**
+     * @return UserAgent
+     */
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    /**
      * @param string $bearerToken
      *
      * @return static $this
@@ -377,6 +395,10 @@ class CurlClient implements ApiClientInterface
     private function prepareHeaders($headers)
     {
         $headers = array_merge($this->defaultHeaders, $headers);
+
+        if ($agent = $this->getUserAgent()) {
+            $headers['YM-User-Agent'] = $agent->getHeaderString();
+        }
 
         if ($this->shopId && $this->shopPassword) {
             $encodedAuth              = base64_encode($this->shopId.":".$this->shopPassword);
